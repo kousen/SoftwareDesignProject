6a2a62113ea729bca6ea9adff64869a5136b5bb4
Ken Kousen <ken.kousen@kousenit.com>
2024-10-01 10:25:40 -0600

    Implement the Classes.kt koans
    Implemented the actual logic for smart casts, sealed classes, and extension functions. Removed commented-out code and unnecessary placeholders for a clearer and more functional module. Added appropriate import renaming for random classes.

--- src/main/kotlin/edu/trincoll/koans/Classes.kt
+++ src/main/kotlin/edu/trincoll/koans/Classes.kt
@@ -1,5 +1,9 @@
 package edu.trincoll.koans
 
+// Rename on import
+import kotlin.random.Random as KRandom
+import java.util.Random as JRandom
+
 // Data classes
 data class Person(val name: String, val age: Int)
 
@@ -8,47 +12,41 @@ fun getPeople(): List<Person> {
 }
 
 fun comparePeople(): Boolean {
-//    val p1 = Person("Alice", 29)
-//    val p2 = Person("Alice", 29)
-//    return p1 == p2  // should be true
-    return false
+    val p1 = Person("Alice", 29)
+    val p2 = Person("Alice", 29)
+    return p1 == p2
 }
 
 // Smart casts
 fun evalSmartCasts(expr: Expr): Int =
     when (expr) {
-        is Num -> TODO()
-        is Sum -> TODO()
-        else -> throw IllegalArgumentException("Unknown expression")
+        is Num -> expr.value
+        is Sum -> evalSmartCasts(expr.left) + evalSmartCasts(expr.right)
     }
 
-interface Expr
+sealed interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
 // Sealed classes
 fun eval(expr: Expr): Int =
     when (expr) {
-        is Num -> TODO()
-        is Sum -> TODO()
-        else -> throw IllegalArgumentException("Unknown expression")
+        is Num -> expr.value
+        is Sum -> eval(expr.left) + eval(expr.right)
     }
 
-// Rename on import
-// import kotlin.random.Random
-// import java.util.Random
 
 fun useDifferentRandomClasses(): String {
     return "Kotlin random: " +
-            // KRandom.nextInt(2) +
+            KRandom.nextInt(2) +
             " Java random:" +
-            // JRandom().nextInt(2) +
+            JRandom().nextInt(2) +
             "."
 }
 
 // Extension functions
-fun Int.r(): RationalNumber = TODO()
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
 
-fun Pair<Int, Int>.r(): RationalNumber = TODO()
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
