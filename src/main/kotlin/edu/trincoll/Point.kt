a8a96942c070780e12a703c6b5d74dd623164900
Ken Kousen <ken.kousen@kousenit.com>
2024-10-03 12:58:22 -0400

    Add polar coordinates support to Point class
    Refactored Point class to a data class and added polarPoint function. This new function allows the creation of points using polar coordinates, improving the flexibility of point creation. Updated the main function to demonstrate both rectangular and polar point instantiation.

--- src/main/kotlin/edu/trincoll/Point.kt
+++ src/main/kotlin/edu/trincoll/Point.kt
@@ -1,16 +1,26 @@
 package edu.trincoll
 
-class Point(
+import kotlin.math.cos
+import kotlin.math.sin
+
+data class Point(
     val x: Double,
     val y: Double
-) {
+)
 
-    fun rectangularPoint(x: Double, y: Double): Point {
-        return Point(x, y)
-    }
+fun rectangularPoint(x: Double, y: Double): Point {
+    return Point(x, y)
+}
+
+fun polarPoint(r: Double, theta: Double) : Point {
+    val x = r * cos(theta)
+    val y = r * sin(theta)
+    return Point(x,y)
 }
 
 fun main() {
-    val point = Point(3.0, 4.0)
-    println(point.rectangularPoint(3.0, 4.0))
+    val pointFromFM = rectangularPoint(3.0, 4.0)
+    println(pointFromFM)
+    val polarPoint = polarPoint(1.0, Math.PI / 2)
+    println(polarPoint)
 }
